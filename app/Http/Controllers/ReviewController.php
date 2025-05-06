785c5c80432980002ded6d07268995c28d42a520
iguchi <iguchi2203@gmail.com>
2025-05-06 11:57:46 +0900

    initial commit

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'content' => 'required'
+        ]);
+        $review = new Review();
+        $review->content = $request->input('content');
+        $review->product_id = $request->input('product_id');
+        $review->user_id = Auth::user()->id;
+        $review->save();
+
+        return back();
+    }
+
+    
+}
